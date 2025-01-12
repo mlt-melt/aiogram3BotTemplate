0147883ba688d661681d8327b219ae647ebc0934
Melt <lmm113@icloud.com>
2025-01-12 23:05:40 +0300

    Added users overview

--- admin_panel/admin/admin.py
+++ admin_panel/admin/admin.py
@@ -1,3 +1,4 @@
+import datetime
 from aiogram import types, F
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command
@@ -13,9 +14,25 @@ from .states import tryFinish
 async def adminCmd(message: types.Message, state: FSMContext):
     if message.from_user.id in admins:
         await tryFinish(state)
-        await message.answer("Админ-панель", reply_markup=admin_mpk())
+        allUsers = User.all()
+        await message.answer(f"Админ-панель\n\nСтатистика:\nЮзеров - {len(allUsers)}", reply_markup=admin_mpk())
 
 @dp.callback_query(F.data == "admin")
 async def adminCall(call: types.CallbackQuery, state: FSMContext):
     await tryFinish(state)
-    await call.message.edit_text("Админ-панель", reply_markup=admin_mpk())
+    allUsers = User.all()
+    await call.message.edit_text(f"Админ-панель\n\nСтатистика:\nЮзеров - {len(allUsers)}", reply_markup=admin_mpk())
+
+@dp.callback_query(F.data.contains("all_users_"))
+async def adminUsersMenuCall(call: types.CallbackQuery, state: FSMContext):
+    pageNum = int(call.data.split("_")[2])
+    allUsers = User.all()
+    currentUsersList = allUsers[((pageNum - 1) * 5):(pageNum * 5)]
+    await call.message.edit_text(f"Все юзеры (страница №{pageNum})\nЮзеров всего - {len(allUsers)}, текущая страница - юзеры {((pageNum - 1) * 5) + 1} - {(pageNum * 5)}", reply_markup=all_users_admin_mpk(pageNum, len(allUsers), currentUsersList))
+
+@dp.callback_query(F.data.contains("checkuser_"))
+async def adminUserMenuCall(call: types.CallbackQuery, state: FSMContext):
+    user_id, pageNum = int(call.data.split("_")[1]), int(call.data.split("_")[2])
+    user = User.get(tg_id=user_id)
+    regDatetime = (user.reg_datetime + datetime.timedelta(hours=3)).strftime("%d/%m/%Y, %H:%M:%S")
+    await call.message.edit_text(f"Юзер {user_id}\n{user.fullname} (@{user.username})\nДата регистрации - {regDatetime}", reply_markup=user_admin_mpk(pageNum))
--- admin_panel/admin/markups.py
+++ admin_panel/admin/markups.py
@@ -1,7 +1,10 @@
+import math
 from aiogram import types
+from db import User
 
 def admin_mpk():
     btns = [
+        [types.InlineKeyboardButton(text="Все юзеры", callback_data=f'all_users_1')],
         [types.InlineKeyboardButton(text="Рассылка", callback_data=f'admin_mailing')],
     ]
     return types.InlineKeyboardMarkup(inline_keyboard=btns)
@@ -9,5 +12,40 @@ def admin_mpk():
 def back_to_admin_panel_mpk():
     btns = [
         [types.InlineKeyboardButton(text="Вернуться в админ-панель", callback_data=f'admin')],
+    ]
+    return types.InlineKeyboardMarkup(inline_keyboard=btns)
+
+def all_users_admin_mpk(pageNum, usersCount, currentUsersList):
+    btns = []
+    currentDisplayed = pageNum * 5
+    for user in currentUsersList:
+        user: User
+        btns.append([types.InlineKeyboardButton(text=f'{user.tg_id} - {user.fullname} (@{user.username})', callback_data=f'checkuser_{user.tg_id}_{pageNum}')])
+    if pageNum != 1:
+        if currentDisplayed < usersCount:
+            btns.append([types.InlineKeyboardButton(text='⏪', callback_data=f'all_users_1'),
+                         types.InlineKeyboardButton(text='◀️', callback_data=f'all_users_{pageNum - 1}'),
+                         types.InlineKeyboardButton(text='▶️', callback_data=f'all_users_{pageNum + 1}'),
+                         types.InlineKeyboardButton(text='⏩', callback_data=f'all_users_{math.ceil(usersCount/5)}')
+                         ])
+        else:
+            btns.append([types.InlineKeyboardButton(text='⏪', callback_data=f'all_users_1'),
+                         types.InlineKeyboardButton(text='◀️', callback_data=f'all_users_{pageNum - 1}'),
+                         types.InlineKeyboardButton(text='▶️', callback_data=f'all_users_{pageNum}'),
+                         types.InlineKeyboardButton(text='⏩', callback_data=f'all_users_{pageNum}')
+                         ])
+    else:
+        if currentDisplayed < usersCount:
+            btns.append([types.InlineKeyboardButton(text='⏪', callback_data=f'all_users_{pageNum}'),
+                         types.InlineKeyboardButton(text='◀️', callback_data=f'all_users_{pageNum}'),
+                         types.InlineKeyboardButton(text='▶️', callback_data=f'all_users_{pageNum + 1}'),
+                         types.InlineKeyboardButton(text='⏩', callback_data=f'all_users_{math.ceil(usersCount/5)}')
+                         ])
+    btns.append([types.InlineKeyboardButton(text='Назад', callback_data=f'admin')])
+    return types.InlineKeyboardMarkup(inline_keyboard=btns)
+
+def user_admin_mpk(pageNum):
+    btns = [
+        [types.InlineKeyboardButton(text="Назад", callback_data=f'all_users_{pageNum}')],
     ]
     return types.InlineKeyboardMarkup(inline_keyboard=btns)
